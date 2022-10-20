7b97c79a152468a386d0a4f3694f964d83989152
MainhattenDev <115190623+MainhattenDev@users.noreply.github.com>
2022-10-20 20:18:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'Mainhattan#9673'
 description 'Extended Discord Rich Presence for FiveM - Made by Mainhattan#9673'
-version '1.0.0'
+version '1.1'
 
 client_scripts {
     'config.lua',
